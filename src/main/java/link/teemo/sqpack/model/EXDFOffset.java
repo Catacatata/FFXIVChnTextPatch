71544036322abfd9278f19400bb260ae6a046f5d
reusu <cq31483192>
2018-01-03 16:38:22 +0800

    fiux replace mode but not runable yet

---
+++ src/main/java/link/teemo/sqpack/model/EXDFOffset.java
@@ -0,0 +1,11 @@
+package link.teemo.sqpack.model;
+
+public class EXDFOffset {
+    public final int index;
+    public final int offset;
+
+    public EXDFOffset(int index, int offset) {
+        this.index = index;
+        this.offset = offset;
+    }
+}
